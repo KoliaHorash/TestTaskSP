b5d1fb3d464296c38b1bacb208c13d8c20411778
KoliaHorash <kolia28011994@gmail.com>
2018-11-06 17:51:27 +0200

    need to add method edit

--- TestTaskSP/Controllers/HomeController.cs
+++ TestTaskSP/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestTaskSP.Models;
 
 namespace TestTaskSP.Controllers
 {
     public class HomeController : Controller
     {
+        FileCotext db = new FileCotext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.FIleInformations);
         }
 
         public ActionResult About()
@@ -25,6 +29,49 @@ namespace TestTaskSP.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FileInformation file, HttpPostedFileBase uploadFile)
+        {
+            if (ModelState.IsValid && uploadFile != null)
+            {
+                string temp = "";
+                string[] arrWord;
+                file.DateDownload = DateTime.Now;
+                file.Path = Server.MapPath("~/Files");
+                var dir = new DirectoryInfo(file.Path);
+                file.FileName = uploadFile.FileName;
+                //Convert.ToString(dir.EnumerateFiles().Select(c => c.Name));
+                file.Size = uploadFile.ContentLength;
+                file.Type = uploadFile.ContentType;
+
+                file.Ip = Request.UserHostAddress;
+                file.UserName = Request.UserHostName;
+
+
+                StreamReader sr = new StreamReader(Path.Combine(file.Path, uploadFile.FileName));
+
+                file.CountSymbols = sr.ReadToEnd().Length;
+                while (sr.EndOfStream != true)
+                {
+                    temp = sr.ReadToEnd();
+                }
+                arrWord = temp.Split(' ');
+
+                file.CountWords = arrWord.Length;
+
+                db.FIleInformations.Add(file);
+                db.SaveChanges();
+
+                return RedirectToAction("UploadFile");
+            }
+            return View(file);
         }
     }
 }
